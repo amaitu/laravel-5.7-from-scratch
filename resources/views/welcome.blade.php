53a119489608cb1067d11afa7793d87e55f79ef4
amaitu <steve@sgbarker.com>
2018-10-28 12:38:17 +0000

    04 - Blade Layout Files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laracasts</title>
-</head>
-<body>
-<h1>
-    Here we go
-</h1>
+@extends('layout')
 
-<p>
-    <a href="/contact">
-        Contact us
-    </a>
-    to learn more.
-</p>
-</body>
-</html>
+@section('content')
+    <h1>My first website</h1>
+@endsection
