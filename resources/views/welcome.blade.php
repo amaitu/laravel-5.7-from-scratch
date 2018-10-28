bcb6b166f9938390edf8eebebd9f5c8f14691815
amaitu <steve@sgbarker.com>
2018-10-28 12:47:44 +0000

    05 - Sending Data to Your Views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,12 @@
 
 @section('content')
     <h1>My first website</h1>
+
+    <ul>
+        @foreach($tasks as $task)
+            <li>
+                {{ $task }}
+            </li>
+            @endforeach
+    </ul>
 @endsection
